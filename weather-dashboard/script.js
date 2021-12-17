40b75e105b04234777d164c65cf2d6cb92438570
loch <nigelloch@gmail.com>
2021-12-17 17:27:39 -0600

    completed current weather

--- weather-dashboard/script.js
+++ weather-dashboard/script.js
@@ -1,19 +1,23 @@
 var searchBtn = document.querySelector("#search-btn")
 
-var textArea = document.querySelector("#text-area")
-
 var apiKey = "673f9746c24ef3e3526fcdc6fe8ef747"
 
-var cityEl = document.getElementById("city-name")
 
 
 
 var searchCity = function() {
 
-    var city = textArea.value
+    var cityEl = document.getElementById("city-name")
 
-    console.log(city)
-    cityEl.textContent = city.toUpperCase()
+    var textArea = document.querySelector("#text-area")
+
+    var city = textArea.value
+        console.log(city)
+        cityEl.textContent = city.toUpperCase()
+   
+       
+    var currDate = moment().format("MM/DD/YYYY");
+        console.log(currDate)
         
 
         // current weather fetch
@@ -21,48 +25,50 @@ var searchCity = function() {
             .then(response => response.json())
             .then(data => {
                 console.log(data)
+
                 var tempEl = document.querySelector("#temp");
-                tempEl.textContent = "Temp-      " + data.main.temp + "F°"
-                var icon = document.querySelector("#icon")
-                icon.setAttribute("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
-                    console.log("http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
-        
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
-                var humidEl = document.querySelector("#humid");
-                    humidEl.textContent = "Humidity-      " + data.main.humidity
-
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
-            .then(response => response.json())
-            .then(data => {
-                console.log(data)
+                tempEl.textContent = "Temp- " + data.main.temp + " F°"
                 var windEl = document.querySelector("#wind");
-                    windEl.textContent = "Wind Speed-      " + data.wind.speed
+                    windEl.textContent = "Wind Speed- " + data.wind.speed + " MPH"
+                var humidEl = document.querySelector("#humid");
+                    humidEl.textContent = "Humidity- " + data.main.humidity + "%"
+                var icon = document.querySelector("#icon")
+                    icon.setAttribute("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+                    console.log("http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
 
+
+
+        // UVI fetch        
         fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=hourly,&appid=" + apiKey)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
                 var uvi = document.querySelector("#uvi")
-                    uvi.textContent = "UV Index-      " + data.current.uvi
+                    uvi.textContent = "UV Index- " + data.current.uvi
 
+                            // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data)
+        //         var tempEl = document.querySelector("#temp");
+        //         tempEl.textContent = "Temp-      " + data.main.temp + "F°"
+        //         var icon = document.querySelector("#icon")
+        //         icon.setAttribute("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+        //             console.log("http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+        
+        // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data)
+        //         var humidEl = document.querySelector("#humid");
+        //             humidEl.textContent = "Humidity-      " + data.main.humidity
 
-            // fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&exclude=hourly,&appid=" + apiKey)
-            // .then(response => response.json())
-            // .then(oneCallData => {
-            //     console.log(oneCallData);
-            //     var uvi = document.querySelector("#uvi")
-            //         uvi.textContent = "UV Index-      " + oneCallData.current.uvi
-            //     var windEl = document.querySelector("#wind");
-            //         windEl.textContent = "Wind Speed-      " + data.wind.speed
-            //     var humidEl = document.querySelector("#humid");
-            //         humidEl.textContent = "Humidity-      " + data.main.humidity
-
-
-
-
+        // fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial")
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         console.log(data)
+        //         var windEl = document.querySelector("#wind");
+        //             windEl.textContent = "Wind Speed-      " + data.wind.speed
 
     
 // pull values from localStorage
@@ -81,9 +87,7 @@ var searchCity = function() {
 
                 })
         })
-})
-})
-}
+    }
 
 
 $(".search-btn").on("click", function() {
